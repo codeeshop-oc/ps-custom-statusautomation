9b80873cbd5b13ab5b5fa8401abd74330331ec20
Anant Negi <anantnegi8@gmail.com>
2025-10-09 09:31:16 +0530

    ps-devtools

--- statusautomation/views/templates/hook/index.php
+++ statusautomation/views/templates/hook/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
@@ -17,7 +18,6 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
-
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
